5c76c779d64d6a60c8ed541ce0bba960cd508709
Mudia Geo <mudiageo@gmail.com>
2024-12-19 13:30:29 +0100

    Notes  page

---
+++ src/lib/utils/navigaing.ts
@@ -0,0 +1,9 @@
+import { goto } from '$app/navigation';
+
+export function navigateWithParams(path, params = {}) {
+  const url = new URL(path, window.location.origin);
+  Object.entries(params).forEach(([key, value]) => {
+    url.searchParams.set(key, value);
+  });
+  return goto(url.pathname + url.search);
+}
